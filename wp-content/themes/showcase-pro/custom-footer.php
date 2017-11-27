1e673dc19ede1cc2984c62cc144a8b2ae3d48215
tugudush <jerome2kph@gmail.com>
2017-11-27 09:39:32 +0000

    updates

--- wp-content/themes/showcase-pro/custom-footer.php
+++ wp-content/themes/showcase-pro/custom-footer.php
@@ -7,6 +7,6 @@
 <div id="footer-social">
 	<a href="https://www.facebook.com/Omniveo-365561523893796"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
 	<!-- <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> -->
-	<!-- <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a> -->
+	<a href="https://www.linkedin.com/company/5067583/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
 	<a href="https://www.youtube.com/channel/UCgVbjuT-jBGLxEFCzPr0bvA"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
 </div><!--/footer-social-->
